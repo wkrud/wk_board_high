1d35dfd8c2d6a79d3edcfe00424ecd79ea5333bd
wkrud <99162203+wkrud@users.noreply.github.com>
2024-10-04 02:14:10 +0900

    git

--- src/main/java/com/wk/boardHigh/test/controller/TestContrller.java
+++ src/main/java/com/wk/boardHigh/test/controller/TestContrller.java
@@ -5,6 +5,7 @@ import com.wk.boardHigh.test.vo.TestVo;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,6 +30,11 @@ public class TestContrller {
     @RequestMapping(value = "/get/port", method = RequestMethod.GET)
     public String check(HttpServletRequest request) {
         return String.format("Server port PORT %s", env.getProperty("local.server.port"));
+    }
+
+    @RequestMapping(value = "/get/ok", method = RequestMethod.GET)
+    public ResponseEntity<String> getPort() {
+        return ResponseEntity.ok("포트 정보");
     }
 
 }
